d8ff57884334a9ad0c53c1c0febb40ece89ecdb9
Guillermo Casanova <guillerizo@hotmail.com>
2016-08-28 18:08:26 +0200

    App uses the library to convert values inserted at the prompt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # roman-numerals
-Roman numbers interpreter (node.js)
+
+Roman numbers interpreter
+
+ - npm install (to download dependencies)
+
+ - npm start (to run application, insert a roman or arabic number and it will be converted)
+ - npm test (to run application tests)
--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log("nothing to do");
+var prompt = require('prompt');
+var RomanNumber = require('./roman-number');
+
+prompt.start();
+ask();
+
+function ask() {
+  prompt.get(['number'], function(err, result) {
+    if (!err) {
+      try {
+        var romanNumber = new RomanNumber(result.number);
+        console.log(romanNumber.toString() + " == " + romanNumber.toInt());
+      } catch (error) {
+        console.log(error);
+      }
+
+      ask();
+    }
+  });
+}
