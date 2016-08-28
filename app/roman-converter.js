d9a3e0a499ad79b10546b34723abfbb2a329c4a9
Guillermo Casanova <guillerizo@hotmail.com>
2016-08-28 17:27:32 +0200

    toRoman function logic and required values tested

--- app/roman-converter.js
+++ app/roman-converter.js
@@ -4,6 +4,18 @@ function toRoman(sArabicNumeral) {
 
   if (typeof sArabicNumeral !== 'number')
     throw new Error('invalid value');
+
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  var result = '';
+  for (var i = 0; i <= decimal.length; i++) {
+    while (sArabicNumeral % decimal[i] < sArabicNumeral) {
+      result += roman[i];
+      sArabicNumeral -= decimal[i];
+    }
+  }
+  return result;
 }
 
 module.exports.toRoman = toRoman;
--- test/roman-converter-test.js
+++ test/roman-converter-test.js
@@ -14,4 +14,28 @@ describe('RomanConverter toRoman tests', function() {
       RomanConverter.toRoman('fas23');
     }).to.throw('invalid value');
   });
+
+  it('Correctly converts number 3 to roman number', function() {
+    expect(RomanConverter.toRoman(3)).equals('III');
+  });
+
+  it('Correctly converts number 4 to roman number', function() {
+    expect(RomanConverter.toRoman(4)).equals('IV');
+  });
+
+  it('Correctly converts number 5 to roman number', function() {
+    expect(RomanConverter.toRoman(5)).equals('V');
+  });
+
+  it('Correctly converts number 1968 to roman number', function() {
+    expect(RomanConverter.toRoman(1968)).equals('MCMLXVIII');
+  });
+
+  it('Correctly converts number 2999 to roman number', function() {
+    expect(RomanConverter.toRoman(2999)).equals('MMCMXCIX');
+  });
+
+  it('Correctly converts number 3000 to roman number', function() {
+    expect(RomanConverter.toRoman(3000)).equals('MMM');
+  });
 });
